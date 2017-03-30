e4ad01a4b94dcd1b454442fad425dc1690e1b92f
kanal <kanal724@gmail.com>
2017-03-31 00:32:47 +0800

    home

--- Ruby/loadexcel/leout1.rb
+++ Ruby/loadexcel/leout1.rb
@@ -2,6 +2,7 @@
 require 'rubygems'
 require 'selenium-webdriver'
 require 'win32ole'
+require 'byebug'
 
 #~ describe "163mail login" do 
 
@@ -18,20 +19,22 @@ require 'win32ole'
 		#worksheet=workbook.worksheets("sheet name") 打开表名
 		#读取excel文件
 		row=2
+		row2=2
 	#~ end
 
 	#~ it "read excel" do
-	while works2.range("a#{row}").value
+	while works2.range("a#{row2}").value
 		
-		eid=works2.range("a#{row}").value.to_s
+		eid=works2.range("a#{row2}")#.value.to_s
 		#~ eid=eidin.value.to_s
-		emsg=works2.range("b#{row}").value.to_s
+		emsg=works2.range("b#{row2}")#.value.to_s
 		#~ emsg=emsgin.value.to_s
 		
 		while worksheet.range("a#{row}").value
 			#选择帐号，把第一列的值做为帐号
 			#~ emailIpt = dr.find_element(:id => 'ctl00_holderLeft_txt_email')
-			emailIpt = dr.find_element(:id => eid)
+			
+			emailIpt = dr.find_element(:id => eid.value.to_s)
 			emailIpt.send_keys worksheet.range("a#{row}").value.to_s
    
 			#选择密码，将第二列值做为密码
@@ -46,14 +49,17 @@ require 'win32ole'
 			
 			#输入错误信息至excel
 			#~ msgopt= dr.find_element(:id,'tip_email').text
-			msgopt= dr.find_element(:id,emsg).text
+			msgopt= dr.find_element(:id,emsg.value.to_s).text
 			worksheet.range("d#{row}").value = msgopt
 			#~ worksheet.range("f#{row}").value = ['=IF(','c#{row}=d#{row}',',"P","F")']
 			#~ puts msgopt.text
 			cc=worksheet.range("c#{row}").value
 			dd=worksheet.range("d#{row}").value
 			ee=worksheet.range("e#{row}")
-		
+
+			#~ if cc==['']
+				#~ break
+			#~ end
 		#~ puts cc
 		#~ puts dd
 		
@@ -76,26 +82,21 @@ require 'win32ole'
 						else
 							ee.value=['F']
 			end
-									
+
+
 			row+=1
-   
 			emailIpt.clear()
 			#~ pwdInput.clear()
    
 			sleep 0.5
 			
-			#~ if cc==['']
-				#~ break
-			#~ end
-			
-			
 		end
 		
-		row+=1
-		puts eid
-		puts emsg
+		puts eid.text
+		puts emsg.text
+		row2+=1
 	end
-		
+
 		workbook.save
 		system ("tskill EXCEL")
 		
--- Ruby/loadexcel/leout2t.rb
+++ Ruby/loadexcel/leout2t.rb
@@ -2,14 +2,15 @@
 require 'rubygems'
 require 'selenium-webdriver'
 require 'win32ole'
+require 'byebug'
 
 #~ describe "163mail login" do 
 
 	#~ before(:all) do
-		#~ dr = Selenium::WebDriver.for :ff
-		#~ url='https://passport.cnblogs.com/register.aspx'
-		#~ dr.navigate.to url
-		#~ sleep 3
+		dr = Selenium::WebDriver.for :ff
+		url='https://passport.cnblogs.com/register.aspx'
+		dr.navigate.to url
+		sleep 3
 		excel = WIN32OLE.new("excel.application")
 		filepath="F:\\autotest\\ruby\\loadexcel\\Data.xlsx" #路径用两斜杠
 		workbook = excel.workbooks.open(filepath)
@@ -18,23 +19,45 @@ require 'win32ole'
 		#worksheet=workbook.worksheets("sheet name") 打开表名
 		#读取excel文件
 		row=2
+		row2=2
 	#~ end
 
-	#~ it "read excel" do
-	while works2.range("a#{row}").value
+	while works2.range("a#{row2}").value
 		
-		eid=works2.range("a#{row}").value.to_s
-		#~ eid=eidin.value.to_s
-		emsg=works2.range("b#{row}").value.to_s
-		#~ emsg=emsgin.value.to_s
+		eid=works2.range("a#{row2}")
+		emsg=works2.range("b#{row2}")
 		
-		row+=1
+		while worksheet.range("a#{row}").value
+
+			emailIpt = dr.find_element(:id => eid.value.to_s)
+			emailIpt.send_keys worksheet.range("a#{row}").value.to_s
+
+			blank= dr.find_element(:class => 'line_tip')
+			blank.click
+
+			msgopt= dr.find_element(:id,emsg.value.to_s).text
+
+			cc=worksheet.range("c#{row}")
+			dd=worksheet.range("d#{row}")
+			ee=worksheet.range("e#{row}")
+
+			row+=1
+			emailIpt.clear()
+			#~ pwdInput.clear()
+   
+			sleep 0.5
+			puts msgopt
+		end
+
+		row2+=1
+
 		
-		puts eid
-		puts emsg
+		puts eid.text
+		puts emsg.text
 	end
 		
 		workbook.save
 		system ("tskill EXCEL")
 		
-		#~ dr.quit
+		dr.quit
+	
