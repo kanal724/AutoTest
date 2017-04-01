0ac4a3e2a89595b0cf74cff9955ddcd27d8da1a7
kanal <kanal724@gmail.com>
2017-04-01 13:27:14 +0800

    Squashed commit of the following:
    commit 8cd62e5b6b0cc8e5631619809904cb4f44fea25f
    Author: kanal <kanal724@gmail.com>
    Date:   Sat Apr 1 13:23:09 2017 +0800
    
        成功做出输入框输入数据的循环
    
    commit 025611b802dc0eca7c0c3fe00f5157c803fda80a
    Author: kanal <kanal724@gmail.com>
    Date:   Fri Mar 31 01:39:12 2017 +0800
    
        成功解决while循环嵌套，但是待解决第二次循环后，excel输入与对比
    
    commit e4ad01a4b94dcd1b454442fad425dc1690e1b92f
    Author: kanal <kanal724@gmail.com>
    Date:   Fri Mar 31 00:32:47 2017 +0800
    
        home
    
    commit 60e71f5fa3c068662e1cf16f34872f8205f63057
    Author: kan <kan_al@qq.com>
    Date:   Thu Mar 30 19:58:22 2017 +0800
    
        循环嵌套
    
    commit a58793d1ab853533404d23444a5689e84807a5ff
    Author: kan <kan_al@qq.com>
    Date:   Thu Mar 30 16:04:36 2017 +0800
    
        update
    
    commit db0c827ffef16cde6a1153eb9b3cf220085298da
    Author: kan <kan_al@qq.com>
    Date:   Thu Mar 30 15:45:25 2017 +0800
    
        添加 大神例子

---
+++ Python/excel-7.py
@@ -0,0 +1,32 @@
+# coding=utf-8
+import pandas as pd
+import time
+from selenium import webdriver
+df1 = pd.read_excel("F:\\AutoTest\\Python\\401.xlsx") # 把excel内容读取为DataFrame对象
+#print (df1)
+#print "执行的excel中总共有%d列数据" %len(df1.columns)
+#print "执行的excel中总共有%d行数据" %len(df1)
+df2 = pd.read_excel("F:\\AutoTest\\Python\\register_Inputbox_ID.xlsx")
+#print (df2)
+#print "ID的excel中总共有%d列数据" %len(df2.columns)
+#print "ID的excel中总共有%d行数据" %len(df2)
+driver = webdriver.Firefox() # 创建webdriver对象，调用火狐
+driver.get("https://passport.cnblogs.com/register.aspx?ReturnUrl=http://home.cnblogs.com") # 访问网站
+# 获取df2中的inputbox_ID的值与相对应的tips_ID的值
+for a in range(0,len(df2)):
+
+    inputbox_ID = df2.iloc[a,0]   # df2中inputbox_ID与tips_ID相对应的取值
+    tips_ID = df2.iloc[a,1]
+    Email_results = list(df1[tips_ID])  # 由df1中相对应的tips_ID创建一个列表
+    def Email(word):
+        driver.find_element_by_id(inputbox_ID).clear()  # 清空搜索框
+        driver.find_element_by_id(inputbox_ID).send_keys(word)  # 输入关键词
+        driver.find_element_by_id("aspnetForm").click()
+        time.sleep(1)  # 等待
+        result = driver.find_element_by_id(tips_ID).text  # 获取提示信息
+        # print (result)
+        return result
+    for index, word in enumerate(list(df1[inputbox_ID])):  # 遍历df1中inputbox_ID下的所有值
+        Email_results[index] = Email(word)  # 把搜索结果写入list
+        df1[tips_ID] = Email_results  # 把list写入df1相对应的tips_ID下
+df1.to_excel("F:\\AutoTest\\Python\\403.xlsx", index=False)  # 把df1另存为excel
---
+++ Ruby/loadexcel/leout2t.rb
@@ -0,0 +1,70 @@
+﻿require 'rspec'
+require 'rubygems'
+require 'selenium-webdriver'
+require 'win32ole'
+require 'byebug'
+
+#~ describe "163mail login" do 
+
+	#~ before(:all) do
+		dr = Selenium::WebDriver.for :ff
+		url='https://passport.cnblogs.com/register.aspx'
+		dr.navigate.to url
+		sleep 3
+		excel = WIN32OLE.new("excel.application")
+		filepath="F:\\autotest\\ruby\\loadexcel\\Data.xlsx" #路径用两斜杠
+		workbook = excel.workbooks.open(filepath)
+		worksheet=workbook.worksheets(1)
+		works2=workbook.worksheets(2)
+		#worksheet=workbook.worksheets("sheet name") 打开表名
+		#读取excel文件
+		row2=2
+	#~ end
+
+	while works2.range("a#{row2}").value
+		
+		eidin=works2.range("a#{row2}")#.value.to_s
+		emsgin=works2.range("b#{row2}")#.value.to_s
+		eid=eidin.value.to_s
+		emsg=emsgin.value.to_s
+		row=2
+		while worksheet.range("a#{row}").value
+
+			emailIpt = dr.find_element(:id => eid)
+			emailIpt.send_keys worksheet.range("a#{row}").value.to_s
+
+			blank= dr.find_element(:class => 'line_tip')
+			blank.click
+
+			msgopt= dr.find_element(:id,emsg).text
+
+			cc=worksheet.range("c#{row}")
+			dd=worksheet.range("d#{row}")
+			ee=worksheet.range("e#{row}")
+
+			row+=1
+			emailIpt.clear()
+			#~ pwdInput.clear()
+   
+			sleep 0.5
+			puts msgopt
+		end
+
+		row2+=1
+
+		
+		puts eidin.text
+		puts emsgin.text
+	end
+		
+		workbook.save
+		system ("tskill EXCEL")
+		
+		dr.quit
+	
+	
+#~ 解决while嵌套问题：
+#~ 关键在于row的使用上出现问题，在每次while循环之前要做row=2，否则内部循环会跳过，（估计-个人猜测：是因为如果将row=1在内循环的while之前不写，或者在外循环的while之前写的话，会判定内循环的while不满足条件，导致不进行循环），并且现在分开row2和row，之前都用row，也是导致内部循环执行完以后，就不再做外部循环的原因
+
+#~ 【LV3】kanho 2017/3/31 1:28:50
+#~ 另外，确实使用了debug后能比较清晰知道是哪里可能出现问题，上网找到的是byebug，（在代码里设置断点（debugger））才会启动
