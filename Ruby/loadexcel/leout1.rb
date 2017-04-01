8cd62e5b6b0cc8e5631619809904cb4f44fea25f
kanal <kanal724@gmail.com>
2017-04-01 13:23:09 +0800

    成功做出输入框输入数据的循环

--- Ruby/loadexcel/leout1.rb
+++ Ruby/loadexcel/leout1.rb
@@ -14,48 +14,54 @@ require 'byebug'
 		excel = WIN32OLE.new("excel.application")
 		filepath="F:\\autotest\\ruby\\loadexcel\\Data.xlsx" #路径用两斜杠
 		workbook = excel.workbooks.open(filepath)
-		worksheet=workbook.worksheets(1)
-		works2=workbook.worksheets(2)
+		idsh=1
+		
+		#~ worksheet=workbook.worksheets(iptsh)
+		works2=workbook.worksheets(idsh)
 		#worksheet=workbook.worksheets("sheet name") 打开表名
 		#读取excel文件
 
-		row2=2
+		eler=2
+		iptsh=2
 	#~ end
 
 	#~ it "read excel" do
-	while works2.range("a#{row2}").value
+	while works2.range("a#{eler}").value
 		
-		eidin=works2.range("a#{row2}")#.value.to_s
+		worksheet=workbook.worksheets(iptsh)
+		
+		eidin=works2.range("a#{eler}")#.value.to_s
 		eid=eidin.value.to_s
-		emsgin=works2.range("b#{row2}")#.value.to_s
+		
+		emsgin=works2.range("b#{eler}")#.value.to_s
 		emsg=emsgin.value.to_s
-		row=2
-		while worksheet.range("a#{row}").value
+		
+		inr=2
+		while worksheet.range("a#{inr}").value
 			#选择帐号，把第一列的值做为帐号
 			#~ emailIpt = dr.find_element(:id => 'ctl00_holderLeft_txt_email')
-			
 			emailIpt = dr.find_element(:id => eid)
-			emailIpt.send_keys worksheet.range("a#{row}").value.to_s
+			emailIpt.send_keys worksheet.range("a#{inr}").value.to_s
    
 			#选择密码，将第二列值做为密码
 			#~ pwdInput= dr.find_element(:id => 'pwdInput')
-			#~ pwdInput.send_keys worksheet.range("b#{row}").value.to_s
+			#~ pwdInput.send_keys worksheet.range("b#{inr}").value.to_s
    
 			#单击登录按钮
 			blank= dr.find_element(:class => 'line_tip')
 			blank.click
 			
-			#~ sleep 1
+			sleep 1
 			
 			#输入错误信息至excel
 			#~ msgopt= dr.find_element(:id,'tip_email').text
 			msgopt= dr.find_element(:id,emsg).text
-			worksheet.range("d#{row}").value = msgopt
-			#~ worksheet.range("f#{row}").value = ['=IF(','c#{row}=d#{row}',',"P","F")']
+			worksheet.range("d#{inr}").value = msgopt
+			#~ worksheet.range("f#{inr}").value = ['=IF(','c#{row}=d#{row}',',"P","F")']
 			#~ puts msgopt.text
-			cc=worksheet.range("c#{row}").value
-			dd=worksheet.range("d#{row}").value
-			ee=worksheet.range("e#{row}")
+			cc=worksheet.range("c#{inr}").value
+			dd=worksheet.range("d#{inr}").value
+			ee=worksheet.range("e#{inr}")
 
 			#~ if cc==['']
 				#~ break
@@ -64,13 +70,13 @@ require 'byebug'
 		#~ puts dd
 		
 		#~ if  cc.eql? dd
-			#~ worksheet.range("e#{row}").value=['P']
+			#~ worksheet.range("e#{inr}").value=['P']
 			#~ elsif cc.nil?
-				#~ worksheet.range("e#{row}").value=['']
+				#~ worksheet.range("e#{inr}").value=['']
 				#~ elsif dd.nil?
-					#~ worksheet.range("e#{row}").value=['']
+					#~ worksheet.range("e#{inr}").value=['']
 					#~ else
-						#~ worksheet.range("e#{row}").value=['F']
+						#~ worksheet.range("e#{inr}").value=['F']
 		#~ end
 
 			if  cc.eql? dd
@@ -83,7 +89,8 @@ require 'byebug'
 							ee.value=['F']
 			end
 
-			row+=1
+			inr+=1
+
 			emailIpt.clear()
 			#~ pwdInput.clear()
    
@@ -93,7 +100,8 @@ require 'byebug'
 		
 		#~ puts eid.text
 		#~ puts emsg.text
-		row2+=1
+		eler+=1
+		iptsh+=1
 	end
 
 		workbook.save
